4a0e6b01f55a82c13a4bcd498a4040d20ea97541
Luke Humphreys <lhumphreys67@gmail.com>
2018-03-03 12:46:38 +0000

    Add OSCPPTools dependency

--- buildDeps.sh
+++ buildDeps.sh
@@ -13,12 +13,36 @@ else
     git clone https://github.com/Grauniad/NanoSecondTimestamps.git deps/NSTimestamps || exit 1
 fi
 
+if [[ -e deps/OSCPPTools ]]; then
+    echo "Existing OSCPPTools directory, no need to clone"
+else
+    git clone https://github.com/Grauniad/OSCPPTools.git deps/OSCPPTools || exit 1
+fi
+
 DEPS_BUILD=$PWD/deps/build
 
 mkdir -p deps/NSTimestamps/build
+mkdir -p deps/OSCPPTools/build
 mkdir -p $DEPS_BUILD
 
+#
+# NSTimestamps
+#
 pushd deps/NSTimestamps || exit 1
+git pull
+pushd build || exit 1
+
+cmake "-DCMAKE_INSTALL_PREFIX:PATH=$DEPS_BUILD" ..
+make -j 3 || exit 1
+make install || exit 1
+
+popd || exit 1
+popd || exit 1
+
+#
+# OSCPPTools
+#
+pushd deps/OSCPPTools || exit 1
 git pull
 pushd build || exit 1
 
