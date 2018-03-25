88973ab8a5ad811a5240bfd38850d448fd530d43
Luke Humphreys <lhumphreys67@gmail.com>
2018-03-25 12:35:04 +0100

    Update to common build deps integration: handle DEPS_ROOT

--- buildDeps.sh
+++ buildDeps.sh
@@ -7,15 +7,19 @@ else
     exit 1
 fi
 
-if [[ -e deps/CMakeUtils ]]; then
+if [[ "$1" == "" ]]; then
+    DEPS_ROOT="$PWD/deps"
+else
+    DEPS_ROOT="$1"
+fi
+if [[ -e $DEPS_ROOT/CMakeUtils ]]; then
     echo "Existing CMakeUtils directory, no need to clone"
 else
-    git clone https://github.com/Grauniad/CMakeUtils.git deps/CMakeUtils || exit 1
+    git clone https://github.com/Grauniad/CMakeUtils.git $DEPS_ROOT/CMakeUtils || exit 1
 fi
 
 declare -A depList
 depList[OSCPPTools]=https://github.com/Grauniad/OSCPPTools.git
 depList[Time]=https://github.com/Grauniad/NanoSecondTimestamps.git
 
-source deps/CMakeUtils/build_tools/buildDepsCommon.sh $@ || exit 1
-
+source $DEPS_ROOT/CMakeUtils/build_tools/buildDepsCommon.sh || exit 1
